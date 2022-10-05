0be46b64da7923cd5c3dc94214b82fb6bc526f3a
root <root@cisner.localdomain>
2022-10-05 17:44:37 -0400

    updated dashboard.js to work with api to calcualte average statistics and reviewResource.java to build the json object using the get and set functions created by the backend in ReviewDto. Cannot build because the classes referenced do not exist on this branch.

--- docs-web/src/main/webapp/src/app/docs/controller/Dashboard.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Dashboard.js
@@ -8,14 +8,23 @@ angular.module('docs').controller('Dashboard', function ($scope, $rootScope, $ti
    * Scope variables.
    */
   $scope.reviews = [];
+  $scope.total = 0;
   $scope.averageGPA = 0;
+  $scope.averageEffort = 0;
+  $scope.averageExperience = 0;
+  $scope.averageSkill = 0;
+
+  /**
+   * Comments?
+   */
 
   /**
    * Load new documents page.
    */
   $scope.loadReviews = function() {
-    Restangular.one('dashboard/list').get().then(function(data) {
-      $scope.reviews = dashboard.reviews;
+    Restangular.one('review/list').get().then(function(data) {
+      $scope.reviews = JSON.parse(data.reviews);
+      $scope.total = data.total;
     });
   };
   $scope.loadReviews();
@@ -25,11 +34,44 @@ angular.module('docs').controller('Dashboard', function ($scope, $rootScope, $ti
    */
   $scope.calculateAverageGPA = function() {
     var sum = 0
-    for (var num of $scope.reviews) {
-      sum += num;
+    for (var review of $scope.reviews) {
+      sum += parseInt(review["GPAScore"]);
     }
-    $scope.averageGPA = sum / $scope.reviews.length;
-  }
+    $scope.averageGPA = ($scope.total == 0 ? 0 : (sum / $scope.total));
+  };
+
+  /**
+   * Average the Effort scores
+   */
+   $scope.calculateAverageEffort = function() {
+    var sum = 0
+    for (var review of $scope.reviews) {
+      sum += parseInt(review["effortScore"]);
+    }
+    $scope.averageEffort = ($scope.total == 0 ? 0 : (sum / $scope.total));
+  };
+
+  /**
+   * Average the Experience scores
+   */
+   $scope.calculateAverageExperience = function() {
+    var sum = 0
+    for (var review of $scope.reviews) {
+      sum += parseInt(review["experienceScore"]);
+    }ope.averageExperience = ($scope.total == 0 ? 0 : (sum / $scope.total));
+  };
+
+  /**
+   * Average the Skill scores
+   */
+   $scope.calculateAverageSkill = function() {
+    var sum = 0
+    for (var review of $scope.reviews) {
+      sum += parseInt(review["skillScore"]);
+    }
+    $scope.averageSkill = ($scope.total == 0 ? 0 : (sum / $scope.total));
+  };
+
   $scope.calculateAverageGPA();
   console.log($scope.averageGPA);
 });

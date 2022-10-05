3b3c73d1aeff7b55f82e0c4ea07e36a1ea6b73f5
alexisaxon <aaxon@andrew.cmu.edu>
2022-10-04 21:24:14 -0400

    created dao file for review object. May still need to create a getReview method

--- docs-core/src/main/java/com/sismics/docs/core/dao/ReviewDao.java
+++ docs-core/src/main/java/com/sismics/docs/core/dao/ReviewDao.java
@@ -1,9 +1,23 @@
 package com.sismics.docs.core.dao;
 
+import com.sismics.docs.core.constant.AuditLogType;
+import com.sismics.docs.core.constant.PermType;
 import com.sismics.docs.core.dao.dto.ReviewDto;
+import com.sismics.docs.core.model.jpa.Document;
+import com.sismics.docs.core.util.AuditLogUtil;
+import com.sismics.util.context.ThreadLocalContext;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
- * Document DAO.
+ * Review DAO.
  * 
  * @author Alexis Axon
  */
@@ -16,6 +30,76 @@ public class ReviewDao {
      * @param reviewer Reviewer
      * @return New ID
      */
+    public String create(Document document, String userId) {
 
+        // Create the UUID
+        review.setReviewId(UUID.randomUUID().toString());
+        review.setCreateDate(new Date());
+        
+        // Create the document
+        EntityManager em = ThreadLocalContext.get().getEntityManager();
+        em.persist(review);
+        
+        // Create audit log
+        AuditLogUtil.create(review, AuditLogType.CREATE, userId);
+        
+        return review.getReviewId();
+    }
+
+    /**
+     * Returns the list of all active reviews.
+     *
+     * @param offset Offset
+     * @param limit Limit
+     * @return List of reviews
+     */
+    public List<Review> findAll(int offset, int limit) {
+        EntityManager em = ThreadLocalContext.get().getEntityManager();
+        TypedQuery<Document> q = em.createQuery("select r from Review r", Review.class);
+        q.setFirstResult(offset);
+        q.setMaxResults(limit);
+        return q.getResultList();
+    }
+
+    /**
+     * Returns the list of all active reviews from a reviewer.
+     * 
+     * @param userId User ID
+     * @return List of reviews
+     */
+    public List<Document> findByUserId(String userId) {
+        EntityManager em = ThreadLocalContext.get().getEntityManager();
+        TypedQuery<Review> q = em.createQuery("select r from Review r where r.reviewerId = :userId", Review.class);
+        q.setParameter("userId", userId);
+        return q.getResultList();
+    }
+
+    /**
+     * Gets an active review by its ID.
+     * 
+     * @param id Review ID
+     * @return Review
+     */
+    public Review getById(String id) {
+        EntityManager em = ThreadLocalContext.get().getEntityManager();
+        TypedQuery<Review> q = em.createQuery("select r from Review r where r.reviewId = :id", Review.class);
+        q.setParameter("id", id);
+        try {
+            return q.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the number of reviews.
+     *
+     * @return Number of reviews
+     */
+    public long getReviewCount() {
+        EntityManager em = ThreadLocalContext.get().getEntityManager();
+        Query query = em.createNativeQuery("select count(r.REV_ID_C) from T_REVIEW r");
+        return ((Number) query.getSingleResult()).longValue();
+    }
 
 }
